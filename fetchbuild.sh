5aee547289b89ed2c63663b5d4aedcb7d1db5333
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2018-10-16 08:47:26 -0700

    fork for projectcoin

--- fetchbuild.sh
+++ fetchbuild.sh
@@ -7,12 +7,26 @@ toolchain=$3
 target_host=$4
 bits=$5
 
-git clone $repo bitcoin
-cd bitcoin
-git checkout $commit
+if [ -d "projectcoin" ]; then
+    # Control will enter here if $DIRECTORY exists.
+    echo "clean up projectcoin folder ... .. ."
+    rm -r projectcoin
+    sleep 2s
+fi
+
+echo "cloning fresh repo ... ... .. . . ."
+git clone $repo projectcoin
+cd projectcoin
+#git checkout $commit
+sleep 2s
+
+echo "copy new default.mk"
+rm depends/hosts/default.mk
+cp /repo/default.mk depends/hosts/
+sleep 2s
 
 patch -p1 < /repo/0001-android-patches.patch
-
+sleep 5s
 
 export PATH=/opt/$toolchain/bin:${PATH}
 export AR=$target_host-ar
@@ -38,8 +52,12 @@ cd ..
 make -j $num_jobs
 make install
 
-$STRIP depends/$target_host/bin/bitcoind
+$STRIP depends/$target_host/bin/projectcoind
 
 repo_name=$(basename $(dirname ${repo}))
 
-tar -zcf /repo/${target_host}_${repo_name}.tar.gz -C depends/$target_host/bin bitcoind
+tar -zcf /repo/${target_host}_${repo_name}.tar.gz -C depends/$target_host/bin projectcoind
+
+echo "we start cleaning in 10 seconds..."
+sleep 10s
+make clean
